b0089f0ba732ab9178e320627f3401a48aebd37b
Dito Hafizh <ditohafizh@outlook.com>
2026-07-29 22:10:40 +0800

    feat: add Master's education details to education section

--- app/data/educations.ts
+++ app/data/educations.ts
@@ -2,12 +2,23 @@ import { Education } from '@/types';
 
 export const EDUCATIONS: Education[] = [
   {
+    school: 'University of Texas at Austin',
+    degree: 'Master of Science in Computer Science',
+    city: 'Austin',
+    country: 'USA',
+    startDate: '2026',
+    endDate: 'Current',
+    description:
+      'Enrolled in the Master of Science in Computer Science (MSCS) program.',
+  },
+  {
     school: 'Electronic Engineering Polytechnic Institute of Surabaya',
     degree: 'B.A.Sc. in Computer Science',
     city: 'Surabaya',
     country: 'Indonesia',
     startDate: '2014',
     endDate: '2018',
-    description: 'Specialized in Data Mining and Machine Learning. Graduated with a 3.40/4.00 GPA. Developed an AI-based predictive system for agricultural optimization as a final project.',
+    description:
+      'Specialized in Data Mining and Machine Learning. Graduated with a 3.40/4.00 GPA. Developed an AI-based predictive system for agricultural optimization as a final project.',
   },
 ];
